c5112a9c55209ef7a9da9952e1a70c87e22e738c
ArthurOzassae <arthur.ozassa@connectcom.com.br>
2023-06-14 23:04:32 -0300

    Versionamento

---
+++ roboExcelRegistro.py
@@ -0,0 +1,48 @@
+# Algoritmo de classificação 
+# Através de registros do Excel, a aplicação entra no registro.br, insere a informação 
+# e se o domínio estiver disponível, grava no TXT.
+# By Arthur Ozassa
+# 10/06/2023
+# Banco origem Excel: dominios.xlsx
+# Destino TXT: resultado.txt
+
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+import time
+import xlrd
+
+print("Iniciando nosso robô...\n")
+arq = open("resultado.txt", "w", encoding="utf-8")
+
+dominios = []
+
+# Abre e lê o Excel
+workbook = xlrd.open_workbook('dominios.xlsx')
+sheet = workbook.sheet_by_index(0)
+
+for linha in range(0, 4):   
+    dominios.append(sheet.cell_value(linha,0))
+
+driver = webdriver.Chrome(executable_path=r"C:\Users\CTC\OneDrive - Connectcom Teleinformatica\Área de Trabalho\RoboPython\Testes\raspatela\chromedriver.exe")
+driver.get("https://registro.br/")
+
+# dominios = ["uol.com.br", "wesleyteste.com.br", "globo.com.br"]
+for dominio in dominios:
+    pesquisa = driver.find_element(By.ID, 'is-avail-field')
+    pesquisa.clear() #Limpando a barra de pesquisa
+    pesquisa.send_keys(dominio)
+    pesquisa.send_keys(Keys.RETURN)
+    time.sleep(2)
+    resultados = driver.find_elements(By.XPATH, '//*[@id="app"]/div/main/div/section/div[2]/div/p')
+    print("Dominio %s %s" % (dominio, resultados[0].text))
+    
+    # Regra para gravar no TXT somente os domínios disponíveis para registro
+    # if resultados[0].text == "Domínio disponível não para registro.":
+    if resultados[0].text == "Domínio disponível para registro.":
+        texto = "Dominio %s %s %s\n" % (dominio, "-" ,resultados[0].text)
+        arq.write(texto)  
+
+time.sleep(5)
+arq.close()
+driver.close()
